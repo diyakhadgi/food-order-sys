fb87ad44e930bbedb67b57448ccff2c0a392f41c
diyakhadgi <khadgidiya8@gmail.com>
2024-02-16 22:55:43 +0545

    login draft

--- login/login.php
+++ login/login.php
@@ -8,7 +8,7 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
 </head>
 <body>
     <div class="container">
@@ -19,22 +19,44 @@ session_start();
     </form>
     <a href="../register/register.php">Don't have an account?</a>
     </div>  
+
     <?php
-      if (isset($_POST['login'])){
+    if (isset($_POST['login'])){
         $username = $_POST['username'];
         $password = md5($_POST['password']);
-        $sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
+        $sql = "SELECT * FROM staff WHERE username = '$username' AND password = '$password'";
         $result = mysqli_query($conn, $sql);
-        $num = mysqli_num_rows($result);
-        if ($num > 0)
-        {
-            $_SESSION['username'] = $username;
-            header("location: http://localhost/food-order-sys/pages/display.php");
-            exit();
+
+        if ($result) {
+            $row = mysqli_fetch_assoc($result);
+            if ($row) {
+                $usertype = $row["usertype"];
+                switch ($usertype) {
+                    case "KITCHEN":
+                        $_SESSION['username'] = $username;
+                        header("location: http://localhost/food-order-sys/pages/display.php");
+                        exit();
+                        break;
+                    case "WAITER":
+                        $_SESSION['username'] = $username;
+                        header("location: ../pages/waiter_home.php");
+                        exit();
+                        break;
+                    case "COUNTER":
+                        $_SESSION['username'] = $username;
+                        header("location: ../pages/counter_home.php");
+                        exit();
+                        break;
+                    default:
+                        echo "Invalid user type";
+                }
+            } else {
+                echo "Invalid username or password";
+            }
         } else {
-            echo "Login failed";
+            echo "Error: " . mysqli_error($conn);
         }
     }
-    ?> 
+    ?>
 </body>
 </html>
