4e346d83115d569fe934f21f666a3be2de5e407b
diyakhadgi <khadgidiya8@gmail.com>
2024-02-19 13:55:58 +0545

    update admin added

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -20,6 +20,10 @@ if ($profile == true) {
             echo $_SESSION['delete'];
             unset($_SESSION['delete']);
         }
+        if(isset($_SESSION['update'])){
+            echo $_SESSION['update'];
+            unset($_SESSION['update']);
+        }
         ?>
         <br><br>
         <a href="add-admin.php" class="btn-primary">Add Admin</a> <br> <br><br>
@@ -50,8 +54,8 @@ if ($profile == true) {
                         <td><?php echo $name ?></td>
                         <td><?php echo $username ?></td>
                         <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="../admin/delete-admin.php?id=<?php echo $id;?>" class="btn-secondary">Delete Admin</a>
+                            <a href="../admin/update-admin.php?id=<?php echo $id;?>" class="btn-secondary">Update Admin</a>
+                            <a href="../admin/delete-admin.php?id=<?php echo $id;?>" class="btn-danger">Delete Admin</a>
                         </td>
                     </tr>
 
---
+++ admin/update-admin.php
@@ -0,0 +1,120 @@
+<?php
+include '../dbcon/dbconnect.php';
+include '../admin/nav.php';
+session_start();
+$profile = $_SESSION['username'];
+if (!$profile) {
+    header("location: http://localhost/food-order-sys/index.html");
+    exit(); 
+}
+?>
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Update Admin</h1>
+        <br><br>
+
+        <?php
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+            $sql = "SELECT * FROM staff WHERE id = $id";
+            $result = mysqli_query($conn, $sql);
+            $num = mysqli_num_rows($result);
+
+            if ($num > 0) {
+                $row = mysqli_fetch_assoc($result);
+
+                $name = $row['name'];
+                $email = $row['email'];
+                $username = $row['username'];
+            } else {
+                header("location: ../admin/manage-admin.php");
+                exit(); 
+            }
+        } else {
+            header("location: ../admin/manage-admin.php");
+            exit(); 
+        }
+        ?>
+        <form action="" method="POST">
+            <table class="tbl-30">
+                <tr>
+                    <td>Full Name: </td>
+                    <td>
+                        <input type="text" name="name" id="" value="<?php echo $name ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Email: </td>
+                    <td>
+                        <input type="email" name="email" id="" value="<?php echo $email ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Username: </td>
+                    <td>
+                        <input type="text" name="username" id="" placeholder="Username" value="<?php echo $username ?>">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Old Password: </td>
+                    <td>
+                        <input type="password" name="password" id="" placeholder="Old Password">
+                    </td>
+                </tr>
+                <tr>
+                    <td>New Password: </td>
+                    <td>
+                        <input type="password" name="npassword" id="" placeholder="New Password">
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <!-- Pass the 'id' as a hidden input field -->
+                        <input type="hidden" name="id" value="<?php echo $id ?>">
+                        <input type="submit" name="submit" value="Update Admin" class="btn-secondary">
+                    </td>
+                </tr>
+
+            </table>
+        </form>
+    </div>
+</div>
+
+<?php
+// Check if the form is submitted
+if (isset($_POST['submit'])) {
+    $id = $_POST['id'];
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = md5($_POST['password']); // Old password provided by the user
+    $npassword = md5($_POST['npassword']); // New password provided by the user
+
+    // Retrieve the old password from the database
+    $sql = "SELECT password FROM staff WHERE id = '$id'";
+    $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_assoc($result);
+    $oldPasswordDB = $row['password']; 
+
+    if (md5($password) == $oldPasswordDB) {
+        // Update the admin's information (name, email, username, and password)
+        $updateQuery = "UPDATE staff SET
+        `name` = '$name',
+        `email` = '$email',
+        `username` = '$username',
+        `password` = '$npassword'
+        WHERE id = '$id'";
+
+        $result = mysqli_query($conn, $updateQuery);
+        if ($result) {
+            $_SESSION['update'] = "Admin Updated";
+            header('location:http://localhost/food-order-sys/admin/manage-admin.php');
+            exit();
+        } else {
+            $_SESSION['update'] = "Failed to update admin";
+        }
+    } else {
+        $_SESSION['update'] = "Old password is incorrect";
+    }
+}
+?>
